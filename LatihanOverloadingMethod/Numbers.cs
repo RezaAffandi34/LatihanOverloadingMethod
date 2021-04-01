52c685dca011d32510cc02d0aaa419d8d26b487c
RezaAffandi34 <reza02affandi@gmail.com>
2021-04-01 18:48:32 +0700

    LatihanOverloadingMethod

---
+++ LatihanOverloadingMethod/Numbers.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace LatihanOverloadingMethod
+{
+    public class Numbers
+    {
+        public int FindMinimum(int number1, int number2)
+        {
+            return 1;
+        }
+
+        public int FindMinimum(int number1, int number2, float number3)
+        {
+            return 2;
+        }
+
+        public int FindMaximum(int number1, int number2)
+        {
+            return 3;
+        }
+
+        public int FindMaximum(int number1, int number2, float number3)
+        {
+            return 4;
+        }
+    }
+}
